f2a5ad371fc300a5e99f503ab43742e66fdb7b80
Zizou10 <paolo.pilati93@gmail.com>
2026-01-17 23:52:04 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,10 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+function getNames() {names.forEach(element => {
+    console.log(element);
+});
+};
+
+getNames();
