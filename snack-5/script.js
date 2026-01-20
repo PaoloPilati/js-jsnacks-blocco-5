4fedc34a5370939b00201adf51f42e58a7a7e3a7
Zizou10 <paolo.pilati93@gmail.com>
2026-01-20 12:08:29 +0100

    Es. 5 risolto con metodo .filter

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,17 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter(element => element % 2 === 0);
+
+//l'arrow function estesa sarebbe 
+// nums.filter(element =>{
+//    if (element % 2 === 0) {
+//        return true;
+//    }
+//    else {
+//        return false;
+//    }
+//});
+
+console.log(evenNums)
